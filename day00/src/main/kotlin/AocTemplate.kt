19cf58651557e0de2b7242afa50ccb4270c9f90e
Karl Wikström <karl@wikstrom.io>
2022-11-30 19:12:21 +0100

    Add parser to template day

--- day00/src/main/kotlin/AocTemplate.kt
+++ day00/src/main/kotlin/AocTemplate.kt
@@ -1,15 +1,32 @@
 import java.io.File
 
+import cc.ekblad.konbini.*
+import cc.ekblad.konbini.ParserResult.Error
+import cc.ekblad.konbini.ParserResult.Ok
+import cc.ekblad.konbini.parseToEnd
+
 fun main() {
-    val input = File("input.txt").readLines().map { it.toInt() }
+    val input = getInput()
     val answer = when (System.getenv("part")) {
         "part2" -> solutionPart2(input)
         else -> solutionPart1(input)
     }
-    println("Kotlin")
     println(answer)
 }
 
-fun solutionPart1(input: List<Int>) = input.sum()
+fun getInput(): List<Long> {
+    val inputFile = File("input.txt").readText()
+    val inputParser = parser{
+        // Add day specific parser here!
+        chain(integer, whitespace).terms
+    }
 
-fun solutionPart2(input: List<Int>) = input.reduce(Int::times)
+    return when (val pRes = inputParser.parseToEnd(inputFile)) {
+        is Ok -> pRes.result
+        is Error -> throw Error("couldn't parse input: " + pRes.reason)
+    }
+}
+
+fun solutionPart1(input: List<Long>) = input.sum()
+
+fun solutionPart2(input: List<Long>) = input.reduce(Long::times)
